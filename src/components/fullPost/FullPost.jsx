4ec4126ceacbdd2f46118e50530239b05d1c428c
Ebrahim780 <akbrly42@gmail.com>
2022-03-25 13:19:45 +0430

    roxo 71-75

--- src/components/fullPost/FullPost.jsx
+++ src/components/fullPost/FullPost.jsx
@@ -11,11 +11,16 @@ class FullPost extends Component {
   componentDidUpdate = () => {
     if (this.props.id)
       if (this.state.loadedPost.id !== this.props.id)
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(Response => {
+        axios.get('posts/' + this.props.id).then(Response => {
           this.setState({ loadedPost: Response.data })
         })
   }
 
+  deletePostHandler = () => {
+    axios.delete('posts/' + this.props.id).then(Response => {
+      console.log(this.state.loadedPost)
+    })
+  }
   render() {
     let post = <p>Please select a Post!</p>;
     if (this.state.loadedPost)
@@ -24,7 +29,8 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete"
+              onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import Post from '../../components/post/Post';
 import FullPost from '../../components/fullPost/FullPost';
 import NewPost from '../../components/newPost/NewPost';
-import axios from 'axios';
+import axios from '../../axios';
 import './Blog.css';
 
 class Blog extends Component {
 
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   }
 
   componentDidMount = () => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(Response => {
-      const posts = Response.data.slice(0, 4)
-      const updatedPosts = posts.map(post => {
-        return {
-          ...post,
-          author: 'Ali'
-        }
+    axios.get('/posts')
+      .then(Response => {
+        const posts = Response.data.slice(0, 4)
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Ali'
+          }
+        })
+        this.setState({ posts: updatedPosts })
       })
-      this.setState({ posts: updatedPosts })
-    })
+      .catch(error => {
+        this.setState({ error: true })
+      })
   }
 
   postSelectedHandler = (id) => {
@@ -30,10 +35,12 @@ class Blog extends Component {
   }
 
   render() {
-    const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} author={post.author}
-        clicked={() => this.postSelectedHandler(post.id)} />
-    })
+    let posts = <p style={{color: 'red'}}>Something went wrong</p>
+    if (!this.state.error)
+      posts = this.state.posts.map(post => {
+        return <Post key={post.id} title={post.title} author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)} />
+      })
     return (
       <div>
         <section className="Posts">
