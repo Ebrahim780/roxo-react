7408a52ea85fdeff763f3365f87e7fc44ebbc337
Ebrahim780 <akbrly42@gmail.com>
2022-03-25 10:20:35 +0430

    remove hamburger

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import BurgerBuilder from './burgerBuilder/BurgerBuilder.jsx';
-import Layout from '../hoc/layout/Layout.jsx'
 
 const App = () => {
   return (
     <>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
     </>
   )
 }
