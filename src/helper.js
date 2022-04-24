8a72dce6cedae901c9c35a6791c10c20cc7d5a02
Ebrahim <akbrly42@gmail.com>
2022-04-24 14:23:14 +0430

    add creative scrollbar

--- src/helper.js
+++ src/helper.js
@@ -12,7 +12,6 @@ export default function calculateWinner(squares) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
-    console.log(a, b, c)
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]
     }
